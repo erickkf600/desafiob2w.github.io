fa7859418c858b64d6d2e262d44ad23b9f4a2e61
erickkf600 <erickkf600@gmail.com>
2019-04-12 21:56:44 -0300

    Build

---
+++ precache-manifest.4059e4790645dcc1038a716c71cc02fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fa297970228a9a65ba1a869dc799b28",
+    "url": "/static/media/back.3fa29797.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "51ea5deecca08522ed35",
+    "url": "/static/js/main.4679d2c5.chunk.js"
+  },
+  {
+    "revision": "640e3c5b83775125b24e",
+    "url": "/static/js/2.f853bede.chunk.js"
+  },
+  {
+    "revision": "51ea5deecca08522ed35",
+    "url": "/static/css/main.fadd7582.chunk.css"
+  },
+  {
+    "revision": "640e3c5b83775125b24e",
+    "url": "/static/css/2.39f1c190.chunk.css"
+  },
+  {
+    "revision": "ee3fbc3ce843c8f3b30c9152d736bec3",
+    "url": "/index.html"
+  }
+];
